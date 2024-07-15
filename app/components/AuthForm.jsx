06b1396a0fe829deecf5f47136eef7e1d921f563
hectorsestanley <hectorsestanley@gmail.com>
2024-07-15 14:29:43 +0100

    update url

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -11,7 +11,7 @@ export default function AuthForm(){
         view="magic_link"
         showLinks={false}
         providers={[]}
-        redirectTo='https://watch-list-plum.vercel.app/auth/callback'
+        redirectTo='https://watch-list-12.vercel.app/'
         appearance={{
             theme: 'dark',
             button: {
